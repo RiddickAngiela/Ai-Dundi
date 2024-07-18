bed38f5f1c467f28d6e2aec77fdec253c9a882e3
Riddick-Angiela <angielariddick@gmail.com>
2024-07-18 17:01:18 +0300

    adding MaterialUi and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <h6>Ready When your ready</h6>
+    <div className="App d-flex justify-content-center align-items-center vh-100 bg-light">
+      <div className="text-center">
+        <h6 className="display-4 text-primary">Ready When You're Ready</h6>
+      </div>
     </div>
   );
 }
