2e9a5e7c4828da35f0b318b5f26e6055a239d884
Riddick-Angiela <angielariddick@gmail.com>
2024-07-19 14:40:46 +0300

    Home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from '@mui/material';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <Container>
-      <Header />
-      <Footer />
-    </Container>
+    <Router>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Container>
+    </Router>
   );
 }
 
